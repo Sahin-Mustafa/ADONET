ed68e20b23e9de56e7c499c9c00790dfde400d4a
Mustafa Sahin <Sahin-Mustafa@gmail.com>
2022-11-30 18:59:34 +0300

    Add project files.

---
+++ ToDo/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ToDo
+{
+    partial class formLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(99, 133);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(31, 43);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.PlaceholderText = "User name";
+            this.txtUserName.Size = new System.Drawing.Size(143, 23);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(31, 90);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.PlaceholderText = "Password";
+            this.txtPassword.Size = new System.Drawing.Size(143, 23);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // formLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(195, 180);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.btnLogin);
+            this.MaximizeBox = false;
+            this.Name = "formLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.formLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnLogin;
+        private TextBox txtUserName;
+        private TextBox txtPassword;
+    }
+}
---
+++ ToDo/Home.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDo
+{
+    public partial class Home : Form
+    {
+        SqlConnection connection = new SqlConnection();
+        SqlCommand command = new SqlCommand();
+        public List<Todo> todos = new List<Todo>();
+        public Todo todo = new Todo();
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            LoadToDosData();
+        }
+
+        private void LoadToDosData()
+        {
+            todos.Clear();
+            lstTodos.Items.Clear();
+            connection.ConnectionString = "Server=DESKTOP-TGAGQ1J\\SQLEXPRESS;Database=ToDoApp;Trusted_Connection=True;";
+            command.Connection = connection;
+            command.CommandText = "SELECT [Id] ,[ToDoTitle] ,[ToDoDetail] ,[ToDoDueDate] ,[Status] FROM [dbo].[ToDos] ";
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            //SELECT [id] ,[UserName] ,[Password] FROM [dbo].[Users]    
+            while (reader.Read())
+            {
+                todo = new Todo
+                {
+                    ToDoId = (int)reader["Id"],
+                    ToDoTitle = reader["ToDoTitle"].ToString(),
+                    ToDoDetail = reader["ToDoDetail"].ToString(),
+                    ToDoDueDate = (DateTime)reader["ToDoDueDate"],
+                    Status = (bool)reader["Status"],
+                };
+                lstTodos.Items.Add(todo);
+                todos.Add(todo);
+            }
+            connection.Close();
+            reader.DisposeAsync();
+        }
+
+        private void txtAdd_Click(object sender, EventArgs e)
+        {
+            if (txtAdd.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Title cannot be left blank.");
+                return;
+            }
+            Todo todo = new Todo();
+            todo.ToDoId = todos.Count + 1;
+            todo.ToDoTitle = txtToDoTitle.Text;
+            todo.ToDoDetail =txtDescription.Text;
+            todo.ToDoDueDate =dtpDueDate.Value;
+            todo.Status = false;
+
+           
+            string query = $"INSERT INTO [dbo].[ToDos] ([Id],[ToDoTitle] ,[ToDoDetail] ,[ToDoDueDate] ,[Status]) VALUES ('{todo.ToDoId}','{todo.ToDoTitle}' ,'{todo.ToDoDetail}' ,'{todo.ToDoDueDate.ToString("yyyy-MM-dd HH:mm:ss.fff")}' ,'{todo.Status}')";
+            command.CommandText = query;
+
+
+
+            connection.Open();
+            int effectedRowsCount = command.ExecuteNonQuery();
+            connection.Close();
+            if (effectedRowsCount > 0) 
+            {
+                LoadToDosData();
+                MessageBox.Show("New ToDo added to list");
+            }
+            else
+                MessageBox.Show("New ToDo didn't add to list");
+            lstTodos.SelectedIndex= -1;
+            
+
+        }
+
+        private void lstTodos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstTodos.SelectedIndex > -1)
+            {
+            todo = (Todo)lstTodos.SelectedItem;
+            //MessageBox.Show($"{todo.ToDoId}");
+            txtUpdateTitle.Text = todo.ToDoTitle;
+            txtUpdatDesc.Text = todo.ToDoDetail;
+            dtpUpdate.Value = todo.ToDoDueDate;
+            cbStatus.Checked = todo.Status;
+            }
+            
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //UPDATE [dbo].[Users] SET [Id] = <Id, int,> ,[UserName] = <UserName, varchar(50),> ,[Password] = <Password, varchar(50),> WHERE <Search Conditions,,>
+            string query = $"UPDATE [dbo].[ToDos] SET [ToDoTitle] = '{txtUpdateTitle.Text}',[ToDoDetail] = '{txtUpdatDesc.Text}',[Status] ='{cbStatus.Checked}' WHERE [Id]={todo.ToDoId}";
+            command.CommandText = query;
+
+            connection.Open();
+            int effectedRowsCount = command.ExecuteNonQuery();
+            connection.Close();
+            if (effectedRowsCount > 0)
+            {
+                MessageBox.Show("Update is successful");
+                LoadToDosData();
+            }
+            else
+            {
+                MessageBox.Show("Update is Unsuccessful");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            //DELETE FROM [dbo].[ToDos] WHERE < Search Conditions,,>
+            if (lstTodos.SelectedIndex > -1)
+            {
+                todo = (Todo)lstTodos.SelectedItem;
+                string query = $"DELETE FROM [dbo].[ToDos] WHERE [Id]={todo.ToDoId}";
+                command.CommandText = query;
+
+                connection.Open();
+                int effectedRowsCount = command.ExecuteNonQuery();
+                connection.Close();
+                if (effectedRowsCount > 0)
+                {
+                    MessageBox.Show("Delet is successful");
+                    LoadToDosData();
+                }
+                else
+                {
+                    MessageBox.Show("Delete is Unsuccessful");
+                }
+            }
+            
+        }
+    }
+}
